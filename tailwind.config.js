a50e72bfd41ed52d556a71ad31e1356450f0833a
darevski <darevski1@egmail.com>
2026-05-14 18:03:54 +0200

    vite config path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
+import flowbitePlugin from "flowbite/plugin";
+
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        foreground: '#000000',
-        'muted-foreground-1': '#6b7280',
-        'muted-foreground-2': '#9ca3af',
-        primary: '#3b82f6',
-        'muted-hover': '#f3f4f6',
-        'muted-focus': '#e5e7eb',
-      }
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                foreground: "#000000",
+                "muted-foreground-1": "#6b7280",
+                "muted-foreground-2": "#9ca3af",
+                primary: "#3b82f6",
+                "muted-hover": "#f3f4f6",
+                "muted-focus": "#e5e7eb",
+            },
+        },
     },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+    plugins: [flowbitePlugin],
+};
